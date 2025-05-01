010d721d807be3fdc31f6fad89447546902987ac
Kalum Gunawardhana <kguna415@gmail.com>
2025-05-01 23:39:42 +0530

    commit

--- src/main/java/edu/demo/service/impl/CustomerServiceImpl.java
+++ src/main/java/edu/demo/service/impl/CustomerServiceImpl.java
@@ -1,7 +1,47 @@
 package edu.demo.service.impl;
 
+import edu.demo.entity.Customer;
+import edu.demo.repository.CustomerRepository;
 import edu.demo.service.CustomerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
 
+@Service
 public class CustomerServiceImpl implements CustomerService {
+    @Autowired
+    private CustomerRepository customerRepository;
 
+    @Override
+    public Customer saveCustomer(Customer customer){
+        return customerRepository.save(customer);
+    }
+
+    @Override
+    public List<Customer> getAllCustomers() {
+        return customerRepository.findAll();
+    }
+
+    @Override
+    public Customer getCustomerById(Long id) {
+        return customerRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Customer updateCustomer(Long id, Customer updatedCustomer) {
+        Customer customer = customerRepository.findById(id).orElse(null);
+        if (customer != null){
+            customer.setAddress(updatedCustomer.getAddress());
+            customer.setName(updatedCustomer.getName());
+            customer.setEmail(updatedCustomer.getEmail());
+            return customerRepository.save(customer);
+        }
+        return null;
+    }
+
+    @Override
+    public void deleteCustomer(Long id) {
+        customerRepository.deleteById(id);
+    }
 }
