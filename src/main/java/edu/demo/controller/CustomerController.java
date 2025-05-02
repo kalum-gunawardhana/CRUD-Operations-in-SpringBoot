9733cab3c8c1d2624b810bc5db3cf88348e0f2c2
Kalum Gunawardhana <kguna415@gmail.com>
2025-05-02 20:18:29 +0530

    commit

--- src/main/java/edu/demo/controller/CustomerController.java
+++ src/main/java/edu/demo/controller/CustomerController.java
@@ -1,5 +1,40 @@
 package edu.demo.controller;
 
-public class CustomerController {
+import edu.demo.entity.Customer;
+import edu.demo.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
+@RestController
+@CrossOrigin
+@RequestMapping("/customers")
+public class CustomerController {
+    @Autowired
+    private CustomerService customerService;
+
+    @PostMapping("/add")
+    public Customer addCustomer(@RequestBody Customer customer){
+        return customerService.saveCustomer(customer);
+    }
+
+    @GetMapping("/get/{id}")
+    public Customer getCustomerById(@PathVariable Long id){
+        return customerService.getCustomerById(id);
+    }
+
+    @GetMapping("/get-all")
+    public List<Customer> getAllCustomers(){
+        return customerService.getAllCustomers();
+    }
+
+    @PutMapping("/update/{id}")
+    public Customer updateCustomer(@PathVariable Long id, @RequestBody Customer updatedCustomer){
+        return customerService.updateCustomer(id, updatedCustomer);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteCustomer(@PathVariable Long id){
+        customerService.deleteCustomer(id);
+    }
 }
--- src/main/java/edu/demo/service/CustomerService.java
+++ src/main/java/edu/demo/service/CustomerService.java
@@ -1,5 +1,12 @@
 package edu.demo.service;
 
-public interface CustomerService {
+import edu.demo.entity.Customer;
+import java.util.List;
 
+public interface CustomerService {
+    Customer saveCustomer(Customer customer);
+    List<Customer> getAllCustomers();
+    Customer getCustomerById(Long id);
+    Customer updateCustomer(Long id, Customer updatedCustomer);
+    void deleteCustomer(Long id);
 }
